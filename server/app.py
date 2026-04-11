cdd406d1b63b9b2b3f3b720c78a428093e6f0bc9
Roma-PaiK <romapai20107@gmail.com>
2026-04-11 16:17:18 +0530

    fix: HF port issues

--- server/app.py
+++ server/app.py
@@ -68,7 +68,7 @@ app = create_fastapi_app(
 def main() -> None:
     """Start the uvicorn server."""
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
 
 
 if __name__ == "__main__":
